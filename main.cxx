a44a59e8a55c899a0487aa144aea558bea4a864e
LuoTat <LuoTat.s@gmail.com>
2024-12-09 09:51:01 +0800

    修改了一下菜单界面

--- main.cxx
+++ main.cxx
@@ -10,12 +10,16 @@ void showMenu()
     std::cout << "        图像处理工具        \n";
     std::cout << "===========================\n";
     std::cout << "请选择功能：\n";
-    std::cout << "1. 将图像转换为灰度图\n";
-    // std::cout << "2. 将图像转换为反转灰度图\n";
-    std::cout << "3. 分离图像通道\n";
-    std::cout << "4. 直方图处理\n";
-    std::cout << "5. 图像平滑处理\n";
-    std::cout << "6. 阈值分割\n";
+    std::cout << "1. BMP文件处理\n";
+    std::cout << "2. 直方图处理\n";
+    std::cout << "3. 空间滤波\n";
+    std::cout << "4. 图像变换\n";
+    std::cout << "5. 阈值分割\n";
+    std::cout << "6. 区域分割\n";
+    std::cout << "7. 边缘检测\n";
+    std::cout << "8. Hough变换\n";
+    std::cout << "9. 区域标记\n";
+    std::cout << "10. 轮廓提取\n";
     std::cout << "0. 退出\n";
     std::cout << "===========================\n";
     std::cout << "请输入数字来运行功能：";
@@ -31,42 +35,28 @@ int main()
 
         if (choice == 1)
         {
+            Mat rgb = imread("../Test/rgb.bmp", IMREAD_UNCHANGED);
+
             // 转换为灰度图像
-            Mat rgb, rgb_gray;
-            rgb = imread("../Test/rgb.bmp", IMREAD_UNCHANGED);
+            Mat rgb_gray;
             cvtColor(rgb, rgb_gray, COLOR_BGR2GRAY);
             imwrite("../Test/rgb_gray.bmp", rgb_gray);
-            std::cout << "已将图像转换为灰度图，保存为 rgb_gray.bmp\n";
-        }
-        // else if (choice == 2)
-        // {
-        //     // 转换为反转灰度图像
-        //     Mat rgb, rgb_gray_inverted;
-        //     rgb = imread("rgb.bmp");
-        //     cvtColor(&rgb, &rgb_gray_inverted, COLOR_BGR2GRAY_INVERTED);
-        //     imwrite("rgb_gray_inverted.bmp", &rgb_gray_inverted);
-        //     printf("已将图像转换为反转灰度图，保存为 rgb_gray_inverted.bmp\n");
-        //     release(&rgb);
-        //     release(&rgb_gray_inverted);
-        // }
-        else if (choice == 3)
-        {
+
+            // 灰度反转
+
             // 分离通道
-            Mat rgb;
             Mat splited_array[3];
-            rgb = imread("../Test/rgb.bmp", IMREAD_UNCHANGED);
             split(rgb, splited_array);
             imwrite("../Test/rgb_B.bmp", splited_array[0]);
             imwrite("../Test/rgb_G.bmp", splited_array[1]);
             imwrite("../Test/rgb_R.bmp", splited_array[2]);
-            std::cout << "已分离图像通道，分别保存为 rgb_B.bmp, rgb_G.bmp, rgb_R.bmp\n";
         }
-        else if (choice == 4)
+        else if (choice == 2)
         {
-            // 直方图处理
-            Mat dim, dim_equalized, dim_equalized_hist, dim_hist, dim_hist_img, dim_equalized_hist_img;
+            Mat dim = imread("../Test/dim.bmp", IMREAD_GRAYSCALE);
+            Mat dim_equalized, dim_equalized_hist, dim_hist, dim_hist_img, dim_equalized_hist_img;
 
-            dim = imread("../Test/dim.bmp", IMREAD_GRAYSCALE);
+            // 直方图均衡化
             equalizeHist(dim, dim_equalized);
 
             int          channels[] = {0};
@@ -81,19 +71,25 @@ int main()
             imwrite("../Test/dim_hist_img.bmp", dim_hist_img);
             imwrite("../Test/dim_equalized_hist_img.bmp", dim_equalized_hist_img);
             imwrite("../Test/dim_equalized.bmp", dim_equalized);
-            std::cout << "已生成直方图均衡化图像，保存为 dim_equalized.bmp\n";
         }
-        else if (choice == 5)
+        else if (choice == 3)
         {
             Mat noise = imread("../Test/noise.bmp", IMREAD_GRAYSCALE);
             Mat noise_medianblur, noise_blur;
 
+            // 中值滤波
             medianBlur(noise, noise_medianblur, 5);
+
+            // 均值滤波
             blur(noise, noise_blur, Size(5, 5));
+
             imwrite("../Test/noise_medianblur.bmp", noise_medianblur);
             imwrite("../Test/noise_blur.bmp", noise_blur);
         }
-        else if (choice == 6)
+        else if (choice == 4)
+        {
+        }
+        else if (choice == 5)
         {
             Mat lena = imread("../Test/lena.bmp", IMREAD_GRAYSCALE);
 
@@ -108,8 +104,13 @@ int main()
             Mat lena_threshold_T, lena_threshold_Iter, lena_threshold_Otsu;
             Mat lena_threshold_T_hist_img, lena_threshold_Iter_hist_img, lena_threshold_Otsu_hist_img;
 
+            // 给定阈值分割
             drawHist_T(lena_hist, lena_threshold_T_hist_img, 4, 1024, static_cast<uchar>(threshold(lena, lena_threshold_T, 80, 255, THRESH_BINARY)));
+
+            // 迭代阈值分割
             // drawHist_T(lena_hist, lena_threshold_Iter_hist_img, 4, 1024, static_cast<uchar>(threshold(lena, lena_threshold_Iter, 114, 255, THRESH_BINARY)));
+
+            // Otsu阈值分割
             drawHist_T(lena_hist, lena_threshold_Otsu_hist_img, 4, 1024, static_cast<uchar>(threshold(lena, lena_threshold_Otsu, 0, 255, THRESH_OTSU)));
 
             imwrite("../Test/lena_threshold_T.bmp", lena_threshold_T);
@@ -122,9 +123,23 @@ int main()
 
             std::cout << "已进行阈值分割.\n";
         }
+        else if (choice == 6)
+        {
+        }
+        else if (choice == 7)
+        {
+        }
+        else if (choice == 8)
+        {
+        }
+        else if (choice == 9)
+        {
+        }
+        else if (choice == 10)
+        {
+        }
         else if (choice == 0)
-            std::cout
-                << "已退出。\n";
+            std::cout << "已退出。\n";
         else std::cout << "输入错误，请重新输入。\n";
     }
     while (choice != 0);
